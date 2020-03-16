be34294b2ab522113f75cf17d9102cb7366f3825
b7Rexx <bij.maharjan@gmail.com>
2020-03-16 14:00:49 +0545

    webpack publicPath fixes

--- webpack.config.js
+++ webpack.config.js
@@ -110,7 +110,7 @@ module.exports = {
     chunkFilename: '[name].chunk-[hash]-[id].js',
     sourceMapFilename: '[name].bundle-[hash]-[id].map',
     path: path.join(__dirname, 'build'),
-    publicPath: '/',
+    publicPath: '/translate_localization/',
   },
   optimization: {
     splitChunks: {
